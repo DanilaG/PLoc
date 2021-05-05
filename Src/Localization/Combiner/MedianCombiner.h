0c48cad16cf376800d910cd54a92cb429ccbae4c
Danila Goriachkin <dndv279@gmail.com>
2021-05-05 20:38:47 +1000

    MedianCombiner is added.

---
+++ Src/Localization/Combiner/MedianCombiner.h
@@ -0,0 +1,33 @@
+#ifndef PLOC_MEDIANCOMBINER_H
+#define PLOC_MEDIANCOMBINER_H
+
+#include "ParallelCombiner.h"
+
+#include <vector>
+
+namespace pl {
+class MedianCombiner: public ParallelCombiner {
+public:
+    MedianCombiner(): ParallelCombiner(std::make_shared<MedianDataCombiner>(),
+                                     std::make_shared<MedianDataCombiner>(),
+                                     std::make_shared<MedianDataCombiner>()) {}
+
+protected:
+    class MedianDataCombiner: public DataCombiner {
+    public:
+        void add(double) final;
+
+        std::optional<double> get() final;
+
+        bool hasValue() final;
+
+        void reset() final;
+
+        ~MedianDataCombiner() {}
+    private:
+        std::vector<double> data_;
+    };
+};
+}
+
+#endif //PLOC_MEDIANCOMBINER_H
