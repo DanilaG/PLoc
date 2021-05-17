88733f1a7993468e1b38847d55ffd1193a6623f7
Danila Goriachkin <dndv279@gmail.com>
2021-05-17 15:04:19 +1000

    Vector method is renamed to QP for tests.

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -55,49 +55,49 @@ TEST(DIRECT_DETECT, FOUR_DETECTORS){
                                                           2),
                            pl::TimePoint<>(0, 4, 3))}
 
-TEST(VECTOR_DETECT, IN_TRIANGLE){
+TEST(QP_DETECT, IN_TRIANGLE){
     LOCALIZATION_RESULT_EQ(pl::localizationByQPMethod({{-5, -5, 15.640312423743286},
                                                        {5,  -5, 15.640312423743286},
                                                        {0,  5,  15.6}},
                                                       10),
                            pl::TimePoint<>(0, -1, 15))}
 
-TEST(VECTOR_DETECT, OUT_TRIANGLE_1){
+TEST(QP_DETECT, OUT_TRIANGLE_1){
     LOCALIZATION_RESULT_EQ(pl::localizationByQPMethod({{1,  1, 11.17851130197758},
                                                        {10, 2, 10.424918292799399},
                                                        {12, 3, 10.25}},
                                                       12),
                            pl::TimePoint<>(15, 3, 10))}
 
-TEST(VECTOR_DETECT, OUT_TRIANGLE_2){
+TEST(QP_DETECT, OUT_TRIANGLE_2){
     LOCALIZATION_RESULT_EQ(pl::localizationByQPMethod({{-10, -15, 13.77307714089059},
                                                        {-2,  -5,  12.927361345041716},
                                                        {-8,  -20, 14.174159928789397}},
                                                       12),
                            pl::TimePoint<>(-5, 30, 10))}
 
-TEST(VECTOR_DETECT, DETECTOR_LINE){
+TEST(QP_DETECT, DETECTOR_LINE){
     LOCALIZATION_RESULT_EQ(pl::localizationByQPMethod({{-1, -1, 4.58113883008419},
                                                        {-2, -2, 4.414213562373095},
                                                        {-4, -4, 5}},
                                                       2),
                            std::nullopt)}
 
-TEST(VECTOR_DETECT, ON_EDGE){
+TEST(QP_DETECT, ON_EDGE){
     LOCALIZATION_RESULT_EQ(pl::localizationByQPMethod({{-1, 0, 3.5},
                                                        {1,  0, 3.5},
                                                        {0,  2, 4}},
                                                       2),
                            pl::TimePoint<>(0, 0, 3))}
 
-TEST(VECTOR_DETECT, BEHIND_DETECTOR){LOCALIZATION_RESULT_EQ(
+TEST(QP_DETECT, BEHIND_DETECTOR){LOCALIZATION_RESULT_EQ(
             pl::localizationByQPMethod(
                     {{-1, 0, 5.061552812808831},
                      {1,  0, 5.061552812808831},
                      {0,  2, 4}}, 2),
     std::nullopt)}
 
-TEST(VECTOR_DETECT, FOUR_DETECTORS){
+TEST(QP_DETECT, FOUR_DETECTORS){
     LOCALIZATION_RESULT_EQ(pl::localizationByQPMethod({{-1, 0, 5.061552812808831},
                                                        {1,  0, 5.061552812808831},
                                                        {0,  2, 4},
