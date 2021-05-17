974e8b519fa3f3c1aa49ebe4b79529cb2d4b6214
Danila Goriachkin <dndv279@gmail.com>
2021-05-17 13:41:27 +1000

    The fastest localization functions are added.

--- Src/Localization/Localization.h
+++ Src/Localization/Localization.h
@@ -10,16 +10,29 @@
 #include "Geometric/Points/TimePoint.h"
 
 namespace pl {
+    std::optional<TimePoint<>> localizationByDirectMethod(const TimePoint<>& p0,
+                                                          TimePoint<> p1,
+                                                          TimePoint<> p2,
+                                                          double c);
     std::optional<TimePoint<>> localizationByDirectMethod(const std::vector<TimePoint<>>& data, double c);
     std::optional<TimePoint<>> localizationByDirectMethod(const std::vector<TimePoint<>>& data,
                                                           double c,
                                                           Combiner& combiner);
 
+    std::optional<TimePoint<>> localizationByQuadrangleMethod(const TimePoint<>& p0,
+                                                              TimePoint<> p1,
+                                                              TimePoint<> p2,
+                                                              TimePoint<> p3,
+                                                              double c);
     std::optional<TimePoint<>> localizationByQuadrangleMethod(const std::vector<TimePoint<>>& data, double c);
     std::optional<TimePoint<>> localizationByQuadrangleMethod(const std::vector<TimePoint<>>& data,
                                                               double c,
                                                               Combiner& combiner);
 
+    std::optional<TimePoint<>> localizationByVectorMethod(const TimePoint<>& p0,
+                                                          TimePoint<> p1,
+                                                          TimePoint<> p2,
+                                                          double c);
     std::optional<TimePoint<>> localizationByVectorMethod(const std::vector<TimePoint<>>& data, double c);
     std::optional<TimePoint<>> localizationByVectorMethod(const std::vector<TimePoint<>>& data,
                                                           double c,
